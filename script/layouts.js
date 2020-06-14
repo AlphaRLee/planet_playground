729992f69af3dabbe9e462be6d048e85d243d97c
R Lee <alpha.r.lee.dev@gmail.com>
2020-06-14 07:59:08 -0600

    Added crosswired layout

--- script/layouts.js
+++ script/layouts.js
@@ -122,6 +122,29 @@ const layouts = [
 		],
 	},
 	{
+		name: "Crosswired",
+		massToRadius: (mass) => 0.165 * Math.sqrt(mass),
+		action: () => [
+			{
+				// Star
+				pos: createVector(windowWidth / 2, windowHeight / 2),
+				mass: 60000,
+				mobile: false,
+				color: color(61, 99, 99),
+			},
+			{
+				pos: createVector(windowWidth / 2, windowHeight / 2 - 250),
+				speed: createVector(400, 0),
+				mass: 8000,
+			},
+			{
+				pos: createVector(windowWidth / 2, windowHeight / 2 + 350),
+				speed: createVector(352, 0),
+				mass: 8000,
+			},
+		],
+	},
+	{
 		name: "Li'l Skipper",
 		action: () => [
 			{
